688ae11622c2af94803de1adfb1a6093d4ff0584
TomiwaOgun <ogunleyetomiwa0@gmail.com>
2025-03-16 19:23:37 -0600

    next.config changes

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {  output: 'export', // Enable static export
     images: {
       unoptimized: true, // Disable image optimization for static export
+      basePath: "/portfolio",
     },};
 
 export default nextConfig;
